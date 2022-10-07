33732ef0fe73b152f4e5988b99ddd1bdb86e7f07
Giancarlo Martini <giancarlomartini@gmail.com>
2022-10-07 18:34:34 +0200

    Modificato il readme

--- client.c
+++ client.c
@@ -1,8 +1,15 @@
-/* 	Semplice programma client che invia un messaggio al server
-    Per compilarlo: gcc client.c -o client
-    Per avviarlo: ./client
-*/
-#include <arpa/inet.h>
+/***************************************************************************
+	file				 : client.c
+    begin                : 2019
+    copyright            : (C) 2019 by Giancarlo Martini and friends
+    email                : gm@giancarlomartini.it
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+*/#include <arpa/inet.h>
 #include <netdb.h>
 #include <netinet/in.h>
 #include <stdio.h>
--- server.c
+++ server.c
@@ -1,8 +1,15 @@
-/* 	Semplice programma server che riceve un messaggio dal client e risponde
-	Per compilare: gcc server.c -o server
-	Per avviarlo: ./server
-*/
+/***************************************************************************
+	file				 : server.c
+    begin                : 2019
+    copyright            : (C) 2019 by Giancarlo Martini and friends
+    email                : gm@giancarlomartini.it
 
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+*/
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
